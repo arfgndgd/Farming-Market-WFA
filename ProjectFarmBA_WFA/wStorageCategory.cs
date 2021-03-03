c4457923be43ea97fa37d4eb4dad8ffe46f4dbad
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-03-03 17:28:19 +0300

    wStorageCategory formu tamamlandı

--- ProjectFarmBA_WFA/wStorageCategory.cs
+++ ProjectFarmBA_WFA/wStorageCategory.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using ProjectFarmBA_WFA.DesingPatterns.SingletonPattern;
+using ProjectFarmBA_WFA.ModelContext;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +15,105 @@ namespace ProjectFarmBA_WFA
 {
     public partial class wStorageCategory : Form
     {
+        ProjectFarmBAEntities db;
         public wStorageCategory()
         {
             InitializeComponent();
+
+            db = DBTool.DBInstance;
+        }
+
+        SqlConnection connection = new SqlConnection("Data Source=LENOVO-PC\\SQLEXPRESS;Initial Catalog=ProjectFarmBA;Integrated Security=True;MultipleActiveResultSets=True");
+
+        private void wStorageCategory_Load(object sender, EventArgs e)
+        {
+            //Çalışan resmini ekleme wForm3
+            pctEmployee.Height = 200;
+            pctEmployee.Width = 200;
+            pctEmployee.SizeMode = PictureBoxSizeMode.StretchImage;
+            lblEmployee.ForeColor = Color.Green;
+            lblEmployee.Text = Login.name + " " + Login.surname;
+
+            try
+            {
+                pctEmployee.Image = Image.FromFile(Application.StartupPath + "\\ImageEmployee\\" + Login.tcno + ".jpg");
+            }
+            catch
+            {
+                pctEmployee.Image = Image.FromFile(Application.StartupPath + "\\ImageEmployee\\resimyok.jpg");
+            }
+
+            //Kategori İşlemleri Sekmesi
+
+            pctStorageCategory.SizeMode = PictureBoxSizeMode.StretchImage;
+            pctStorageCategory.Width = 200;
+            pctStorageCategory.Height = 200;
+            pctStorageCategory.BorderStyle = BorderStyle.Fixed3D;
+
+            StorageCategoryShow();
+        }
+        private void StorageCategoryShow()
+        {
+            try
+            {
+                connection.Open();
+                SqlDataAdapter categoryList = new SqlDataAdapter("select ID , CategoryName as [Kategori Adı], Description as [Açıklama], [Veri Yaratma Tarihi] , [Veri Güncelleme Tarihi] , [Veri Silme Tarihi] ,[Veri Durumu] from StorageCategories", connection);
+                DataSet dataSet = new DataSet();
+                categoryList.Fill(dataSet);
+                dGVStorageCategory.DataSource = dataSet.Tables[0];
+
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Farming Market", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                connection.Close();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            bool searchData = false;
+            if (txtCategoryName.Text.Length > 0)
+            {
+                connection.Open();
+                SqlCommand selectQuery = new SqlCommand("select * from StorageCategories where StorageCategoryName = '" + txtCategoryName.Text + "'", connection);
+                SqlDataReader dataReader = selectQuery.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    searchData = true;
+                    lblID.Text = dataReader.GetValue(0).ToString();
+                    txtCategoryName.Text = dataReader.GetValue(1).ToString();
+                    lblDescription.Text = dataReader.GetValue(2).ToString();
+                    try
+                    {
+                        pctStorageCategory.Image = Image.FromFile(Application.StartupPath + "\\ImageStorageCategory\\" + dataReader.GetValue(1).ToString() + ".jpg");
+
+                    }
+                    catch
+                    {
+                        pctStorageCategory.Image = Image.FromFile(Application.StartupPath + "\\ImageStorageCategory\\resimyok.jpg");
+                    }
+                    lblCreated.Text = dataReader.GetValue(3).ToString();
+                    lblUpdated.Text = dataReader.GetValue(4).ToString();
+                    lblDeleted.Text = dataReader.GetValue(5).ToString();
+                    lblStatus.Text = dataReader.GetValue(6).ToString();
+
+                    break;
+                }
+
+                if (searchData == false)//Kayıt yoksa
+                {
+                    MessageBox.Show("Aranan kayıt bulunamadı", "Farming Market", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+                connection.Close();
+            }
+            else
+            {
+                MessageBox.Show("Lütfen kayıtlarda olan bir Ambar Kategori ismi giriniz", "Farming Market", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
