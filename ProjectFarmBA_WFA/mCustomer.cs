e7df7d7d87453ae3daa7cfd17643d4eb81738966
Arif Gündoğdu <gundogduarif4@gmail.com>
2021-03-08 02:34:56 +0300

    Okey

---
+++ ProjectFarmBA_WFA/mCustomer.cs
@@ -0,0 +1,33 @@
+﻿using ProjectFarmBA_WFA.DesingPatterns.SingletonPattern;
+using ProjectFarmBA_WFA.ModelContext;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectFarmBA_WFA
+{
+    public partial class mCustomer : Form
+    {
+        ProjectFarmBAEntities db;
+        public mCustomer()
+        {
+            InitializeComponent();
+            db = DBTool.DBInstance;
+        }
+
+        //TODO: Customer Storage elden alışveriş hallet
+
+        SqlConnection connection = new SqlConnection("Data Source=LENOVO-PC\\SQLEXPRESS;Initial Catalog=ProjectFarmBA;Integrated Security=True;MultipleActiveResultSets=True");
+        private void mCustomer_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
